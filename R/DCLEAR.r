54a8f4e729cf6f9a47d756105658cb2117e229ae
Wuming Gong <gongx030@umn.edu>
2020-02-24 18:31:27 -0600

    .

---
+++ R/DCLEAR.r
@@ -0,0 +1,19 @@
+#' DCLEAR
+#'
+#' @import tensorflow
+#' @import keras
+#' @import tfprobability
+#' @import Matrix
+#' @import SummarizedExperiment
+#' @importFrom matrixStats rowSds rowVars rowMedians rowMins rowMaxs
+#' @import futile.logger
+#' @importFrom phangorn NJ treedist RF.dist
+#' @importFrom igraph as.igraph distances graph.adjacency ends incident_edges adjacent_vertices degree permute.vertices vcount V contract simplify set_vertex_attr graph.empty add_edges add_vertices
+#' @importFrom purrr map
+#' @importFrom stringr str_pad
+#' @importFrom abind abind
+#' @importFrom Biostrings BStringSet oligonucleotideFrequency
+#' @importFrom dplyr filter sample_n mutate select collect
+#' @importFrom fields rdist
+#'
+NULL
