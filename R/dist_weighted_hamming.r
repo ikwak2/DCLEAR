4aff53c511d349dfbc1da12b4c791b5ed137038b
ikwak2 <ilyoup.kwak@gmail.com>
2020-02-29 13:14:10 +0900

    update

--- R/dist_weighted_hamming.r
+++ R/dist_weighted_hamming.r
@@ -1,44 +1,102 @@
-setGeneric('dist_weighted_hamming', function(x, y, ...) standardGeneric('dist_weighted_hamming'))
+#'
+#' dist_weighted_hamming
+#'
+#' implementation of weighted hamming algorithm
+#'
+#' @param x Sequence object of 'phyDat' type.
+#'
+#' @param wVec Weight vector for the calculation of weighted hamming distance
+#'
+#' @param dropout Different weighting strategy is taken to consider interval dropout with dropout = 'TRUE'. Default is, dropout = 'FALSE'.
+#'
+#' @return Calculated distance matrix of input sequences. The result is a 'dist' class object.  
+#'
+#' @author Il-Youp Kwak
+#'
+#' @examples
+#'
+#' set.seed(1)
+#' mu_d1 = c( 30, 20, 10, 5, 5, 1, 0.01, 0.001)
+#' mu_d1 = mu_d1/sum(mu_d1)
+#' simn = 100 # number of cell samples
+#' m = 200  ## number of targets
+#' sD = sim_seqdata(sim_n = simn, m = m, mu_d = 0.03, d = 12, n_s = length(mu_d1), outcome_prob = mu_d1, p_d = 0.005 )
+#' ## RF score with hamming distance
+#' sD$seqs %>% dist.hamming()  %>% NJ() %>% RF.dist(sD$tree, normalize = TRUE)
+#' ## RF score with weighted hamming
+#' InfoW = -log(mu_d1)
+#' InfoW[1:2] = 1
+#' InfoW[3:7] = 4.5
+#' sD$seqs %>% `dist_weighted_hamming(InfoW) %>% NJ() %>% RF.dist(sD$tree, normalize = TRUE)
+#' ## RF score with weighted hamming, cosidering dropout situation
+#' nfoW = -log(mu_d1)
+#' InfoW[1] = 1
+#' InfoW[2] = 12
+#' InfoW[3:7] = 3
+#' sD$seqs %>% dist_weighted_hamming(InfoW, dropout=TRUE) %>% NJ() %>% RF.dist(sD$tree, normalize = TRUE)
+#'
+#' 
+
+setGeneric('dist_weighted_hamming', function(x, wVec, dropout, ...) standardGeneric('dist_weighted_hamming'))
 
 setMethod(
 	'dist_weighted_hamming',
 	signature(
-		x = 'phyDat',
-		y = 'missing'
+            x = 'phyDat',
+#            y = 'missing',
+            wVec = 'numeric',
+            dropout = 'logical'
 	),
-	function(x, y, method, ...){
+	function(x, y, wVec, dropout=FALSE, ...){
 
-    # compute pairwise weighted hamming distance 
+            ## compute pairwise weighted hamming distance
+
+            states <- c('0', '-', LETTERS)
+            states2num = 1:length(states)
+            names(states2num) = states
+            x2 = do.call('rbind', x %>% map(~states2num[.]) )
+            
+            if (dropout) {
+                D = dist_w_ham2(x2, wVec)
+            } else {
+                D = dist_w_ham(x2, wVec)
+            }
+            
+            D = (D + t(D)) / max(D)
+            rownames(D) = colnames(D) = names(x)
+            return(as.dist(D) )
 
 	}
 )
 
-setMethod(
-	'dist_weighted_hamming',
-	signature(
-		x = 'phyDat',
-		y = 'phyDat'
-	),
-	function(x, y, method, ...){
+
+
+#setMethod(
+#	'dist_weighted_hamming',
+#	signature(
+#		x = 'phyDat',
+#		y = 'phyDat'
+#	),
+#	function(x, y, method, ...){
 
     # compute the weighted hamming distance between two matrix x and y
 
-	}
-)
+#	}
+#)
 
 #' compute the weighted hamming distance between two matrices
 #
-setMethod(
-	'dist_weighted_hamming',
-	signature(
-		x = 'phyDat',
-		y = 'matrix'
-	),
-	function(x, y, method, ...){
-
-		num_states <- nlevels(x)
-		y <- y %>% phyDat(type = 'USER', levels = levels(x))
-		dist_weighted_hamming(x, y, method, ...)
-
-	}
-)
+#setMethod(
+#	'dist_weighted_hamming',
+#	signature(
+#		x = 'phyDat',
+#		y = 'matrix'
+#	),
+#	function(x, y, method, ...){
+#
+#		num_states <- nlevels(x)
+#		y <- y %>% phyDat(type = 'USER', levels = levels(x))
+#		dist_weighted_hamming(x, y, method, ...)
+#
+#	}
+#)
