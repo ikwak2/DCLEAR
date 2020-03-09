81fe69ff254969c7d7b597770ea1a4a94a638794
Wuming Gong <gongx030@umn.edu>
2020-03-09 15:59:06 -0500

    update

--- R/sample_kmer.r
+++ R/sample_kmer.r
@@ -9,32 +9,24 @@ setMethod(
   ),
   function(
     x,
-    n_nodes = 10L,  # number of randomly sampled nodes (leaves or internval nodes)
     k = 1L  # k-mer,
   ){
 
-    # sampling nodes
-#    s <- x@x %>%
-#      length() %>%
-#      sample(n_nodes)
-
-    s <- which(degree(x@graph, mode = 'out') == 0)
-
-    X <- x@x[s] %>% as.character()
+    X <- x@x %>% as.character()
 
     p <- expand.grid(
-      from = 1:n_nodes, 
-      to = 1:n_nodes, 
+      from = 1:nrow(X),
+      to = 1:nrow(X),
       start = 1:(x@n_targets - k + 1)
      ) %>%
       filter(from < to)
 
     D <- x@graph %>% distances(
-      v = names(x@x)[s], 
-      to = names(x@x)[s]
+      v = names(x@x),
+      to = names(x@x)
     )
     d <- D[cbind(p[, 'from'], p[, 'to'])]
-    # the k-mer at each position for each pairs of sample
+
     str_from <- do.call('paste0', lapply(1:k, function(j) X[cbind(p[, 'from'], p[, 'start'] + j - 1)]))
     str_to <- do.call('paste0', lapply(1:k, function(j) X[cbind(p[, 'to'], p[, 'start'] + j - 1)]))
 
@@ -42,7 +34,9 @@ setMethod(
       from = str_from,
       to = str_to,
       distance = d
-    )
+    ) %>%
+     group_by(from, to, distance) %>%
+     tally()
   }
 
 ) # sample_kmer  
