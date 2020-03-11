598fb0fdbac73a53968a58c3a3bb104f5d1f9269
Wuming Gong <gongx030@umn.edu>
2020-03-11 10:48:47 -0500

    init

---
+++ R/substr_kmer.r
@@ -0,0 +1,32 @@
+setGeneric('substr_kmer', function(x, ...) standardGeneric('substr_kmer'))
+
+setMethod(
+  'substr_kmer',
+  signature(
+    x = 'kmer_summary'
+  ),
+  function(
+    x,
+    k = 2
+  ){
+
+    if (k >= x@k)
+      stop(sprintf('k=%d must be smaller than the input k=%d', k, x@k))
+
+    x@df <- do.call('rbind', lapply(seq_len(x@k - k + 1), function(start){
+      x@df %>%
+        ungroup() %>%
+        mutate(
+          from = substr(from, start, start + k - 1),
+          to = substr(to, start, start + k - 1)
+        )
+    })) %>%
+      group_by(from, to, distance) %>%
+      summarize(n = sum(n))
+
+    x@kmers <-   do.call('paste0', do.call('expand.grid', lapply(1:k, function(j) x@alphabets)))
+    x@k <- k
+    x
+  }
+)
+
