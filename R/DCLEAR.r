a4bd5a165dcaad7c8916ea5bc3b5e61de527f0f3
Wuming Gong <gongx030@umn.edu>
2020-06-03 18:01:09 -0500

    update

--- R/DCLEAR.r
+++ R/DCLEAR.r
@@ -11,7 +11,6 @@
 #' @importFrom igraph as.igraph distances graph.adjacency ends incident_edges adjacent_vertices degree permute.vertices vcount V contract simplify set_vertex_attr graph.empty add_edges add_vertices
 #' @importFrom purrr map
 #' @importFrom stringr str_pad
-#' @importFrom abind abind
 #' @importFrom Biostrings BStringSet oligonucleotideFrequency
 #' @importFrom BiocParallel bplapply
 #' @importFrom phangorn phyDat
