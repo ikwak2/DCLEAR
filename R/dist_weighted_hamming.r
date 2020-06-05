621eb7589478bb482ea719341cbf63271852d64c
Wuming Gong <gongx030@umn.edu>
2020-06-05 12:06:08 -0500

    clean up

--- R/dist_weighted_hamming.r
+++ R/dist_weighted_hamming.r
@@ -76,34 +76,3 @@ setMethod(
 	}
 )
 
-
-
-#setMethod(
-#	'dist_weighted_hamming',
-#	signature(
-#		x = 'phyDat',
-#		y = 'phyDat'
-#	),
-#	function(x, y, method, ...){
-
-    # compute the weighted hamming distance between two matrix x and y
-
-#	}
-#)
-
-#' compute the weighted hamming distance between two matrices
-#
-#setMethod(
-#	'dist_weighted_hamming',
-#	signature(
-#		x = 'phyDat',
-#		y = 'matrix'
-#	),
-#	function(x, y, method, ...){
-#
-#		num_states <- nlevels(x)
-#		y <- y %>% phyDat(type = 'USER', levels = levels(x))
-#		dist_weighted_hamming(x, y, method, ...)
-#
-#	}
-#)
