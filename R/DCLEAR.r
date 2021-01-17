fe7b82410796eb6e23107930419fce54182a8966
Wuming Gong <gongx030@umn.edu>
2021-01-16 18:12:33 -0600

    Revert "update"
    This reverts commit fc66110e385d822d4beba3b31fd6f4f752e2f29c.

--- R/DCLEAR.r
+++ R/DCLEAR.r
@@ -7,6 +7,7 @@
 #' @import methods
 #' @import Matrix
 #' @import dplyr
+#' @import tfdatasets
 #' @importFrom matrixStats rowLogSumExps
 #' @importFrom igraph distances graph.adjacency degree permute.vertices vcount V contract simplify set_vertex_attr
 #' @importFrom purrr map
@@ -17,6 +18,8 @@
 #' @importFrom Rcpp evalCpp
 #' @importFrom ape write.tree read.tree
 #' @importFrom tidyr replace_na
+#' @importFrom keras keras_model_custom save_model_weights_tf load_model_weights_tf
+#' @importFrom tensorflow shape
 #' @useDynLib DCLEAR
 #' @docType package
 #' @name DCLEAR
