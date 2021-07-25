35c786cae9bbcc42329c37ea9e57db16a6fa5ca5
Wuming Gong <gongx030@umn.edu>
2021-07-25 16:14:53 -0500

    init

---
+++ R/as_igraph.r
@@ -0,0 +1,21 @@
+#' as_igraph
+#' 
+#' Convert an phylo object to an igraph object, while keeping the weight (in contrast to igraph::as.igraph)
+#' @param x a phylo object
+#' @return an igraph object
+#' 
+#' @export
+#'
+setMethod(
+	'as_igraph',
+	signature(
+		x = 'phylo'
+	),
+	function(x){
+		n <- length(x$tip.label) + x$Nnode
+		g <- graph_from_edgelist(x$edge)
+		V(g)$name <- c(x$tip.label, sprintf('Node%d', 1:x$Nnode))
+		E(g)$weight <- x$edge.length
+		g
+	}
+)
